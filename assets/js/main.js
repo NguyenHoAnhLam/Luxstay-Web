0e8a753e4923746aadfb73d0cdcd088b499978f1
NguyenHoAnhLam <nguyenhoanhlam161120000@gmial.com>
2022-05-18 11:01:01 +0700

    Luxstay web commit

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+const bookingGuest = document.querySelector('.booking-guest');
+const popoverGuest = document.querySelector('.guest-popover'); 
+const allWrapper = document.querySelector('.wrapper');
+const popoverLanguage =document.querySelector('.popover-language');
+const languageSelect = document.querySelector('.language')
+
+
+function showGuestSelcect(){
+    // e.preventDefault();
+    popoverGuest.classList.add('open')
+    bookingGuest.classList.add('hover')
+}
+function hideGuestSelcect(){
+    // e.preventDefault();
+    popoverGuest.classList.remove('open')
+    popoverLanguage.classList.remove('open')
+    bookingGuest.classList.remove('hover')
+}
+
+function showLanguageSelcect(){
+    popoverLanguage.classList.toggle('open');
+}
+
+bookingGuest.addEventListener('click',showGuestSelcect);
+allWrapper.addEventListener('click',hideGuestSelcect);
+bookingGuest.onclick = function(e){
+    e.stopPropagation();
+};
+languageSelect.addEventListener('click',showLanguageSelcect);
+languageSelect.onclick = function(e){
+    e.stopPropagation();
+}
+
+
+// slider
+var count = 1;
+setInterval(function(){
+    document.getElementById('radio' + count).checked = true;
+    count ++;
+    if(count > 3){
+        count = 1;
+    }
+},5000);
